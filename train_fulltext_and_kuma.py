559e29c41d3f1f2dbb63afaab6bc93a9d6c70f13
casszhao <zhixue.zhao@sheffield.ac.uk>
2022-05-17 22:27:00 +0100

    m

--- train_fulltext_and_kuma.py
+++ train_fulltext_and_kuma.py
@@ -6,7 +6,13 @@ import os
 import argparse
 import logging
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("running on the GPU")
+else:
+    device = torch.device("cpu")
+    print("running on the CPU")
 CUDA_VISIBLE_DEVICES = 0
 
 
